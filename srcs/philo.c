c3b5efe2319710c58b01ac5aeb774398523abf0a
Andor Gunczer <agunczer@2-B-4.42heilbronn.de>
2022-02-17 13:54:06 +0100

    norm done

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,63 +6,11 @@
 /*   By: agunczer <agunczer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 12:59:08 by agunczer          #+#    #+#             */
-/*   Updated: 2022/02/17 13:00:30 by agunczer         ###   ########.fr       */
+/*   Updated: 2022/02/17 13:52:11 by agunczer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
-
-static int	ft_isspace(char c)
-{
-	if (c == ' ')
-		return (1);
-	else if ((c == '\n' || c == '\t' || c == '\v' || c == '\t' || c == '\r'
-			|| c == '\f'))
-		return (1);
-	else
-		return (0);
-}
-
-int	ft_atoi(const char *str)
-{
-	int		i;
-	int		j;
-	int		num;
-
-	i = 0;
-	j = 0;
-	num = 0;
-	while (ft_isspace(*(str + i)) == 1 && str[i] != '\0')
-	{
-		i++;
-		j++;
-	}
-	if (str[i] == '\0')
-		return (0);
-	if (*(str + j) == '-' || *(str + j) == '+')
-		i++;
-	while (*(str + i) && *(str + i) >= '0' && *(str + i) <= '9')
-	{
-		num = num * 10 + (*(str + i) - '0');
-		i++;
-	}
-	if (*(str + j) == '-')
-		num *= -1;
-	return (num);
-}
-
-int	is_num(char *str)
-{
-	if (str == NULL)
-		return (0);
-	while (*str != '\0')
-	{
-		if (*str > 57 || *str < 48)
-			return (0);
-		str++;
-	}
-	return (1);
-}
 
 int	destroy_mutexes(t_philo *philo)
 {
@@ -110,7 +58,7 @@ int	fill_philosophers(t_input *input, t_philo *philo, int i)
 	while (i < input->num_of_philo)
 	{
 		philo[i].mutex_rfork = (pthread_mutex_t *)
-		malloc(sizeof(pthread_mutex_t));
+			malloc(sizeof(pthread_mutex_t));
 		if (pthread_mutex_init(philo[i].mutex_rfork, NULL))
 			return (1);
 		i++;
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,17 +6,11 @@
 /*   By: agunczer <agunczer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 14:09:44 by agunczer          #+#    #+#             */
-/*   Updated: 2022/02/17 12:54:45 by agunczer         ###   ########.fr       */
+/*   Updated: 2022/02/17 13:51:56 by agunczer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
-
-// void	time_init(t_time *time)
-// {
-// 	gettimeofday(&(time->last_meal), NULL);
-// 	gettimeofday(&(time->time_now_death), NULL);
-// }
 
 int	ft_log(t_philo *philo, char *event, int exception, char *ptr)
 {
@@ -57,5 +51,57 @@ int	one_philo(t_philo *philo)
 	now = get_time();
 	ft_log(philo, " has died", 0, NULL);
 	*philo->death = 1;
+	return (1);
+}
+
+static int	ft_isspace(char c)
+{
+	if (c == ' ')
+		return (1);
+	else if ((c == '\n' || c == '\t' || c == '\v' || c == '\t' || c == '\r'
+			|| c == '\f'))
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		j;
+	int		num;
+
+	i = 0;
+	j = 0;
+	num = 0;
+	while (ft_isspace(*(str + i)) == 1 && str[i] != '\0')
+	{
+		i++;
+		j++;
+	}
+	if (str[i] == '\0')
+		return (0);
+	if (*(str + j) == '-' || *(str + j) == '+')
+		i++;
+	while (*(str + i) && *(str + i) >= '0' && *(str + i) <= '9')
+	{
+		num = num * 10 + (*(str + i) - '0');
+		i++;
+	}
+	if (*(str + j) == '-')
+		num *= -1;
+	return (num);
+}
+
+int	is_num(char *str)
+{
+	if (str == NULL)
+		return (0);
+	while (*str != '\0')
+	{
+		if (*str > 57 || *str < 48)
+			return (0);
+		str++;
+	}
 	return (1);
 }
